c4d72ecfd2db156d591097bb3a7ff5616f4a689d
ame diphoko <diphokoame@gmail.com>
2021-07-08 20:46:24 +0200

    update adverse events validations

--- esr21_subject_validation/form_validators/adverse_event_form_validator.py
+++ esr21_subject_validation/form_validators/adverse_event_form_validator.py
@@ -7,22 +7,36 @@ class AdverseEventFormValidator(FormValidator):
 
     def clean(self):
         cleaned_data = self.cleaned_data
+        self.validate_experienced_ae()
         self.validate_ae_end_date(cleaned_data=cleaned_data)
         self.validate_outcome(cleaned_data=cleaned_data)
         self.validate_maae()
+
+    def validate_experienced_ae(self):
+        fields = ['ae_name', 'event_details', 'meddra_pcode', 'meddra_version',
+                  'meddra_pname', 'start_date', 'status', 'ae_grade',
+                  'study_treatmnt_rel', 'nonstudy_treatmnt_rel',
+                  'studyproc_treatmnt_rel', 'action_taken', 'outcome',
+                  'serious_event', 'special_interest_ae', 'medically_attended_ae',
+                  'treatment_given', 'ae_study_discontinued', 'covid_related_ae']
+        for field in fields:
+            self.required_if(
+                YES,
+                field='experienced_ae',
+                field_required=field)
 
     def validate_ae_end_date(self, cleaned_data=None):
         self.required_if(
             'resolved',
             field='status',
-            field_required='resolution_date',
+            field_required='stop_date',
             required_msg='AE is resolved, please provide the AE end date')
 
         start_date = cleaned_data.get('start_date')
-        end_date = cleaned_data.get('resolution_date', None)
+        end_date = cleaned_data.get('stop_date', None)
 
         if end_date and end_date < start_date:
-            message = {'resolution_date':
+            message = {'stop_date':
                        'AE end date can not be before AE start date'}
             self._errors.update(message)
             raise ValidationError(message)
