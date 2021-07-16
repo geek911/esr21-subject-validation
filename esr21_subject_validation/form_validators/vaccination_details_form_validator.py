4dd7878a72f9e05d1d0e357277072bbcd90ab793
Moses Chawawa <moseschawawa@gmail.com>
2021-07-16 14:02:04 +0200

    - added more tests and refactored unit tests - checked for PEP8 Complience

--- esr21_subject_validation/form_validators/vaccination_details_form_validator.py
+++ esr21_subject_validation/form_validators/vaccination_details_form_validator.py
@@ -1,33 +1,49 @@
 from edc_constants.constants import YES, NO
 from edc_form_validators import FormValidator
 from .crf_form_validator import CRFFormValidator
-from ..constants import SECOND_DOSE
+from ..constants import FIRST_DOSE
 
 
 class VaccineDetailsFormValidator(CRFFormValidator, FormValidator):
 
     def clean(self):
 
-        self.required_if(YES, field="received_dose", field_required="is_received_dose")
+        self.required_if(YES,
+                         field="received_dose",
+                         field_required="is_received_dose")
 
-        self.required_if(YES, field="received_dose", field_required="vaccination_site")
+        self.required_if(YES,
+                         field="received_dose",
+                         field_required="vaccination_site")
 
-        self.required_if(YES, field="received_dose", field_required="vaccination_date")
+        self.required_if(YES,
+                         field="received_dose",
+                         field_required="vaccination_date")
 
-        self.required_if(YES, field="received_dose", field_required="admin_per_protocol")
+        self.required_if(YES,
+                         field="received_dose",
+                         field_required="admin_per_protocol")
 
-        self.required_if(NO, field="admin_per_protocol", field_required="reason_not_per_protocol")
+        self.required_if(NO,
+                         field="admin_per_protocol",
+                         field_required="reason_not_per_protocol")
 
-        self.required_if(YES, field="received_dose", field_required="lot_number")
+        self.required_if(YES,
+                         field="received_dose",
+                         field_required="lot_number")
 
-        self.required_if(YES, field="received_dose", field_required="expiry_date")
+        self.required_if(YES,
+                         field="received_dose",
+                         field_required="expiry_date")
 
-        self.required_if(YES, field="received_dose", field_required="provider_name")
+        self.required_if(YES,
+                         field="received_dose",
+                         field_required="provider_name")
 
-        self.validate_other_specify(field="location", other_specify_field="location_other")
+        self.validate_other_specify(field="location")
 
-        self.not_required_if(SECOND_DOSE, field="is_received_dose", field_required="next_vaccination")
-
-        # TODO: next_vaccination if time-point enrollment is (Week 0)
+        self.required_if(FIRST_DOSE,
+                         field="is_received_dose",
+                         field_required="next_vaccination_data")
 
         super().clean()
