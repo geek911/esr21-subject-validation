0351fd42f066addfa26f40983cc26e60c7e29ebc
MoffatMore <mofenyimoffat@gmail.com>
2021-09-12 03:07:22 +0200

    add esr21 screening eligibility form validations

---
+++ esr21_subject_validation/form_validators/screening_eligibility_form_validator.py
@@ -0,0 +1,24 @@
+
+
+from django.apps import apps as django_apps
+from django.core.exceptions import ValidationError
+from edc_form_validators import FormValidator
+
+
+class ScreeningEligibilityFormValidator(FormValidator):
+
+    edc_protocol = django_apps.get_app_config('edc_protocol')
+
+    @property
+    def screening_eligibility_cls(self):
+        return django_apps.get_model(self.screening_eligibility_model)
+
+    def clean(self):
+
+        report_datetime = self.cleaned_data.get('report_datetime')
+        if (report_datetime and self.edc_protocol.study_open_datetime > report_datetime):
+            message = {
+                'report_datetime': ('Date cannot be before study starts. Study opened on'
+                                    f' {self.edc_protocol.study_open_datetime.date()}.')}
+            self._errors.update(message)
+            raise ValidationError(message)
